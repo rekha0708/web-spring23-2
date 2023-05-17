0c4d15d2e0bb4c0d144bfb8474d189ee342b015f
001405649_RekhaYadav <yrekha0820@gmail.com>
2023-05-17 15:27:55 -0700

    Update hw1.js

--- homework/js/hw1.js
+++ homework/js/hw1.js
@@ -1,12 +1,24 @@
 function ageDiscount(age){
     if (age >= 65) {
-      //console.log("Special Discount");
       return "Special Discount"
     } else {
-    //console.log("Not Allowed"); 
       return "Not Allowed"
     }
   }
 
- let result = ageDiscount(23); 
+ let result = ageDiscount(23);
  console.log(result);
+
+ function multiply100(numbers) {
+    const arr1 = [];
+    numbers.forEach(number => {
+      arr1.push(number * 100);
+    });
+    return arr1;
+  }
+
+  const numbers = [1, 2, 3];
+  const arr1 = multiply100(numbers);
+
+  console.log(multipliedNumbers); // [100, 200, 300]
+  //console.log(numbers);
